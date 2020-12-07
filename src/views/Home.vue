269c7e4e627fa5c1dbac4ecd06f0daccaff520b2
Eugene Kim <eugene@MacBook-Pro.local>
2020-12-07 15:15:11 -0800

    Update with modified code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <b-container>
+    <b-row align-v="center">
+      <b-col>
+        <b-card
+        title="This is a card"
+        img-src="https://picsum.photos/600/300/?image-25"
+        img-alt="Job-info"
+        img-top
+        tag="article"
+        style="max-width:20rem"
+        class="mb-2"
+        >
+          <b-card-text>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem quisquam adipisci tempore exercitationem dolore velit minus voluptas beatae et labore dolores maxime deleniti nobis, consequatur voluptate laboriosam vero, molestias nam.
+          </b-card-text>
+          <b-button variant="primary">Apply</b-button>
+        </b-card>
+      </b-col>
+      <b-col>
+        <b-card
+        title="This is a card"
+        img-src="https://picsum.photos/600/300/?image-25"
+        img-alt="Job-info"
+        img-top
+        tag="article"
+        style="max-width:20rem"
+        class="mb-2"
+        >
+          <b-card-text>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem quisquam adipisci tempore exercitationem dolore velit minus voluptas beatae et labore dolores maxime deleniti nobis, consequatur voluptate laboriosam vero, molestias nam.
+          </b-card-text>
+          <b-button variant="primary">Apply</b-button>
+        </b-card>
+      </b-col>
+      <b-col>
+        <b-card
+        title="This is a card"
+        img-src="https://picsum.photos/600/300/?image-25"
+        img-alt="Job-info"
+        img-top
+        tag="article"
+        style="max-width:20rem"
+        class="mb-2"
+        >
+          <b-card-text>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem quisquam adipisci tempore exercitationem dolore velit minus voluptas beatae et labore dolores maxime deleniti nobis, consequatur voluptate laboriosam vero, molestias nam.
+          </b-card-text>
+          <b-button variant="primary">Apply</b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
   }
 };
 </script>
